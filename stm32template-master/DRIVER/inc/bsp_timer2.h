d6cb3a8fc7746288550ebe3d1988d682a0024933
Xiaofan <2839252107@qq.com>
2017-04-21 22:36:09 +0800

    del mqtt

--- stm32template-master/DRIVER/inc/bsp_timer2.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BSP_TIMER2_H
-#define __BSP_TIMER2_H
-
-#include "stm32f10x.h"
-
-void TIM2_Int_Init(u16 arr,u16 psc);
-
-#endif
--- stm32template-master/DRIVER/inc/bsp_timer3.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BSP_TIMER3_H
-#define __BSP_TIMER3_H
-#include "stm32f10x.h"
-
-void TIM3_Int_Init(u16 arr,u16 psc);
-void TIM3_IRQHandler(void);
-
-#endif
-
